2c9223d093d61bb13ae8280ab86a079c082b836f
Mahmudur Rahman <raianmr@gmail.com>
2023-09-05 05:50:46 +0600

    add config

---
+++ scraper/main.py
@@ -0,0 +1,21 @@
+import json
+from settings import settings
+
+
+if __name__ == '__main__':
+    # print(json.dumps(settings.model_dump(), indent=4))
+    query = """
+query {
+  viewer {
+    login
+  }
+  rateLimit {
+    limit
+    cost
+    remaining
+    resetAt
+  }
+}
+    """.strip()
+    
+    
---
+++ scraper/settings.py
@@ -0,0 +1,19 @@
+﻿from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        extra='ignore',
+        env_file_encoding='utf-8-sig',
+        case_sensitive=False,
+    )
+
+    db_host: str
+    db_username: str
+    db_password: str
+    db_name: str
+    gh_token: str
+
+
+settings = Settings()
