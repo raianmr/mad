d0e1fd15adde1a63edec09fc9db866318b754f03
Mahmudur Rahman <raianmr@gmail.com>
2024-01-16 06:18:50 +0600

    add 20-MAD db factory

--- MAD20/models/db.py
+++ MAD20/models/db.py
@@ -7,15 +7,13 @@ from sqlalchemy.orm import sessionmaker
 DB_URL = f"{settings.DB_TYPE}://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
 engine = create_engine(DB_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True) 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 
 
+@contextmanager
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-
-get_db_ctx = contextmanager(get_db)
